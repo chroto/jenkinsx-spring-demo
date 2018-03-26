675278e9caf4a8cf3d8eb0ef52eaecfd1c603d5b
Chris Proto <chris.proto@devopsgorilla.com>
2018-03-25 23:57:40 -0600

    Use RestController annotation

--- src/main/kotlin/com/example/jenkinsxspringdemo/HomeController.kt
+++ src/main/kotlin/com/example/jenkinsxspringdemo/HomeController.kt
@@ -1,11 +1,13 @@
 package com.example.jenkinsxspringdemo
 
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-class HomeController {
 
-    @RequestMapping("/")
-    fun home() = "Hello"
+
+@RestController
+class AppController {
+
+	@RequestMapping("/")
+	fun index() = "This is home!"
 }
